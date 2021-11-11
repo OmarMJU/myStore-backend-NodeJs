27671e4688025e2a6965519630b44e242cff4bb1
Omar M Juarez <omarmju906p@hotmail.com>
2021-11-10 22:23:02 -0600

    Se agregan más rutas de prueba.

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get("/", (request, response) => {
     response.send("Soy tu servidor en Express");
 });
 
+app.get("/contact", (req, res) => {
+    res.send("Seccion de contacto");
+});
+
+app.get("/products", (req, res) => {
+    res.json({
+        products: [
+            {name: "Microblading", price: 2500},
+            {name: "Lifting de pestañas", price: 600},
+            {name: "Facial", price: 300},
+            {name: "Maquillaje", price: 1500}
+        ]
+    });
+});
+
 app.listen(_PORT, () => {
     console.log("Escuchando desde el puerto", _PORT);
 });
