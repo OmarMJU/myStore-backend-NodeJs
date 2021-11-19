fef146bc10d4df410354e965769e15f557fc46bf
Omar M Juarez <omarmju906p@hotmail.com>
2021-11-18 23:46:16 -0600

    Versionados a la URL.

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,16 @@
+const express = require("express");
+
 const routerUsers = require("./users.router");
 const routerProducts = require("./products.router");
 const routerCategories = require("./categories.router");
 
 function routerApi(app) {
-    app.use("/products", routerProducts);
-    app.use("/categories", routerCategories);
-    app.use("/users", routerUsers);
+    const router = express.Router();
+
+    app.use("/api/v1", router);
+    router.use("/products", routerProducts);
+    router.use("/categories", routerCategories);
+    router.use("/users", routerUsers);
 }
 
 module.exports = routerApi;
