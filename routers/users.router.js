915a45a15fb57833129880a048a5bf9eac45c8c4
Omar M Juarez <omarmju906p@hotmail.com>
2021-11-24 01:30:16 -0600

    Endpoints de usuarios completos.

--- routers/users.router.js
+++ routers/users.router.js
@@ -1,53 +1,68 @@
 const express = require("express");
+const faker = require("faker");
+
 const routerUsers = express.Router();
 
-/* Endpoints users */
+// Todos los usuarios.
 routerUsers.get("/", (req, res) => {
-    const { limit, offset} = req.query;
+    const { limit } = req.query;
+    const size = limit || 20;
+    const users = [];
 
-    if (limit && offset) {
-        res.status(200).json({
-            limit: limit,
-            offset: offset
+    for (let i = 0; i < size; i++) {
+        users.push({
+            id: faker.datatype.uuid(),
+            name: faker.name.findName(),
+            gender: faker.name.gender(),
+            email: faker.internet.email()
         });
-    } else {
-        res.json([
-            { name: "Daniela", lastName: "Zamudio", age: "30", mail: "danni_zam@mail.com" },
-            { name: "Manoli", lastName: "Hernandez", age: "35", mail: "mano_her35@mail.com" },
-            { name: "Pamela", lastName: "Rosas", age: "52", mail: "pam.rosas@mail.com" },
-            { name: "Janneth", lastName: "Carmona", age: "45", mail: "jan-car@mail.com" }
-        ]);
     }
+
+    res.status(200).json(users);
 });
 
+// Usuario por id.
 routerUsers.get("/:id", (req, res) => {
     const { id } = req.params;
+
     res.status(200).json({
         id,
-        name: "Daniela",
-        lastName: "Zamudio",
-        age: "30",
-        mail: "danni_zam@mail.com"
+        name: faker.name.findName(),
+        gender: faker.name.gender(),
+        email: faker.internet.email()
     });
 });
 
-routerUsers.get("/:id/shopping", (req, res) => {
+// Crear Usuario
+routerUsers.post("/", (req, res) => {
+    const datas = req.body;
+
+    res.status(201).json({
+        message: "User created",
+        id: faker.datatype.uuid(),
+        datas
+    });
+});
+
+// Actualizar usuario
+routerUsers.patch("/:id", (req, res) => {
     const { id } = req.params;
-    res.status(200).json({
-        idUser: id,
-        shopping: [
-            { name: "Micoblading", price: 2500, date: "14/10/2021" },
-            { name: "Facial", price: 300, date: "05/11/2021" }
-        ]
+    const datas = req.body;
+
+    res.status(201).json({
+        message: "User updated",
+        id,
+        datas
     });
 });
 
-routerUsers.get("/:idUser/shopping/:idShop", (req, res) => {
-    const { idUser, idShop } = req.params;
-    res.status(200).json({
-        idUser,
-        idShop,
-        shop: { name: "Micoblading", price: 2500, date: "14/10/2021" }
+// Eliminar usuario.
+routerUsers.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    res.status(201).json({
+        id,
+        message: "User deleted"
     });
 });
 
