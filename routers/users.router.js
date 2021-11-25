062d996eac1230bda16d0eaf9da324d95dd6e3c0
Omar M Juarez <omarmju906p@hotmail.com>
2021-11-25 01:32:04 -0600

    Asincronia para usuarios.

--- routers/users.router.js
+++ routers/users.router.js
@@ -6,22 +6,30 @@ const routerUsers = express.Router();
 const service = new UserService();
 
 // Todos los usuarios.
-routerUsers.get("/", (req, res) => {
-    const users = service.getAll();
+routerUsers.get("/", async (req, res) => {
+    const users = await service.getAll();
     res.status(200).json(users);
 });
 
 // Usuario por id.
-routerUsers.get("/:id", (req, res) => {
+routerUsers.get("/:id", async (req, res) => {
     const { id } = req.params;
-    const user = service.getOne(id);
-    res.status(200).json(user);
+
+    try {
+        const user = await service.getOne(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({
+            error: "Error to get user",
+            message: error
+        });
+    }
 });
 
 // Crear Usuario
-routerUsers.post("/", (req, res) => {
+routerUsers.post("/", async (req, res) => {
     const datas = req.body;
-    const userCreate = service.create(datas);
+    const userCreate = await service.create(datas);
 
     res.status(201).json({
         message: "User created",
@@ -30,26 +38,42 @@ routerUsers.post("/", (req, res) => {
 });
 
 // Actualizar usuario
-routerUsers.patch("/:id", (req, res) => {
+routerUsers.patch("/:id", async (req, res) => {
     const { id } = req.params;
     const datas = req.body;
-    const userUpdate = service.update(id, datas);
 
-    res.status(201).json({
-        message: "User updated",
-        userUpdate
-    });
+    try {
+        const userUpdate = await service.update(id, datas);
+
+        res.status(201).json({
+            message: "User updated",
+            userUpdate
+        });
+    } catch (error) {
+        res.status(404).json({
+            error: "Error to update user",
+            message: error
+        });
+    }
 });
 
 // Eliminar usuario.
-routerUsers.delete("/:id", (req, res) => {
+routerUsers.delete("/:id", async (req, res) => {
     const { id } = req.params;
-    const userDelete = service.delete(id);
 
-    res.status(201).json({
-        message: "User deleted",
-        userDelete
-    });
+    try {
+        const userDelete = await service.delete(id);
+
+        res.status(201).json({
+            message: "User deleted",
+            userDelete
+        });
+    } catch (error) {
+        res.status(404).json({
+            error: "Error to delete user",
+            message: error
+        });
+    }
 });
 
 module.exports = routerUsers;
--- services/users.service.js
+++ services/users.service.js
@@ -23,53 +23,81 @@ class UserService {
     }
 
     // Obtiene todos los usuarios.
-    getAll() {
-        return this.users;
+    async getAll() {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(this.users);
+            }, 5000);
+        });
     }
 
     // Obtiene un usuario por Id.
-    getOne(id) {
-        return this.users.filter(item => item.id === id);
+    async getOne(id) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const index = this.users.findIndex(user => user.id === id);
+
+                if (index === -1) {
+                    reject("User not found");
+                } else {
+                    const userFind = this.users.filter(item => item.id === id);
+                    resolve(userFind);
+                }
+            }, 3000);
+        });
     }
 
     // Crea un usuario.
-    create(datas) {
-        const newProduct = {
-            id: faker.datatype.uuid(),
-            ...datas
-        };
+    async create(datas) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const newProduct = {
+                    id: faker.datatype.uuid(),
+                    ...datas
+                };
 
-        this.users.push(newProduct);
-        return newProduct;
+                this.users.push(newProduct);
+                resolve(newProduct);
+            }, 3000);
+        });
     }
 
     // Actualizar usuario.
-    update(id, dataChanges) {
-        const index = this.users.findIndex(index => index.id === id);
+    async update(id, dataChanges) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const index = this.users.findIndex(index => index.id === id);
 
-        if(index === -1) {
-            throw new Error("User not found");
-        }
+                if(index === -1) {
+                    reject("User not found");
+                } else {
+                    const userUpdate = this.users[index];
+                    this.users[index] = {
+                        ...userUpdate,
+                        ...dataChanges
+                    };
 
-        const userUpdate = this.users[index];
-        this.users[index] = {
-            ...userUpdate,
-            ...dataChanges
-        };
-
-        return this.users[index];
+                    const productUpdate =  this.users[index];
+                    resolve(productUpdate);
+                }
+            }, 4000);
+        });
     }
 
     // Eliminar usuario.
-    delete(id) {
-        const index = this.users.findIndex(item => item.id === id);
+    async delete(id) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                const index = this.users.findIndex(item => item.id === id);
 
-        if(index === -1) {
-            throw new Error("User not found");
-        }
-
-        this.users.splice(index, 1);
-        return { id };
+                if(index === -1) {
+                    reject("User not found");
+                } else {
+                    this.users.splice(index, 1);
+                    resolve({ id });
+                }
+            }, 2000);
+        });
     }
 }
 
