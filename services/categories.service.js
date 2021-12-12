bec9412d493e3bbf6453a7bd9d61fdb52035ca2c
Omar M Juarez <omarmju906p@hotmail.com>
2021-12-11 19:14:20 -0600

    Categorias con Squelize.

--- services/categories.service.js
+++ services/categories.service.js
@@ -1,71 +1,81 @@
-const boom = require("@hapi/boom");
-const pool = require("../libs/postgres");
+// const boom = require("@hapi/boom");
+// const pool = require("../libs/postgres");
+const { QueryTypes } = require("sequelize/dist");
+const sequelize = require("../libs/sequelize");
 
 class CategoriesServie {
-
-    // Constuctor de la clase.
-    constructor() {
-       this.pool = pool;
-       this.pool.on("error", err => console.error(err));
-    }
 
     // Todas las categorias.
     async getAll() {
         const query = "SELECT * FROM categories";
-        const categories = await this.pool.query(query);
-        return categories.rows;
+        const [data] = await sequelize.query(query);
+        return data;
     }
 
     // Una categorìa por id.
     async getOne(id) {
-        const query = "SELECT * FROM categories where id = $1";
-        const category = await this.pool.query(query, [id]);
-        return category.rows;
+        const query = "SELECT * FROM categories where id = ?";
+        const [data] = await sequelize.query(query, {
+                replacements: [id],
+                type: QueryTypes.SELECT
+            }
+        );
+
+        return data;
     }
 
     // Crea una categoria.
     async create(datas) {
         let { name, items } = datas;
         const queryId = "SELECT (MAX(ID) + 1) AS ID FROM CATEGORIES";
-        const { rows } = await this.pool.query(queryId);
+        const [findedId] = await sequelize.query(queryId);
+        const { id } = findedId[0];
 
         if (!items) {
             items = 0;
         }
 
-        const values = [rows[0].id, name, items];
-        const query = "INSERT INTO CATEGORIES (ID, NAME, ITEMS) VALUES ($1, $2, $3)";
-        await this.pool.query(query, values);
+        const query = "INSERT INTO CATEGORIES (ID, NAME, ITEMS) VALUES (:id, :name, :items)";
+        await sequelize.query(query, {
+            replacements: {
+                id: id,
+                name: name,
+                items: items
+            },
+        });
 
         return {
-            id: rows[0].id,
-            ...datas
+            id,
+            ...datas,
         };
     }
 
     // Actualiza una categorìa.
     async update(id, dataChange) {
-        const datasUpdate = [];
-        const setQuery = [];
-
-        Object.entries(dataChange).forEach((entrie, index) => {
-            setQuery.push(entrie[0] + ` = $${index + 1}`);
-            datasUpdate.push(entrie[1]);
-        });
-
-        const query = `UPDATE CATEGORIES SET ${setQuery.join(", ")} WHERE ID = ${id}`;
-        await this.pool.query(query, datasUpdate);
-
-        return {
+        const datasQuery = [];
+        const updateDatas = {
             id,
             ...dataChange
         };
+
+        Object.keys(dataChange).forEach(key => datasQuery.push(`${key} = :${key}`));
+        const query = `UPDATE CATEGORIES SET ${datasQuery.join(", ")} WHERE ID = :id`;
+        await sequelize.query(query, {
+            replacements: updateDatas
+        });
+
+        return updateDatas;
     }
 
     // Borrar categoria
     async delete(id) {
-        const query = "DELETE FROM CATEGORIES WHERE ID = $1";
-        await this.pool.query(query, [id]);
+        const query = "DELETE FROM CATEGORIES WHERE ID = :id";
+        await sequelize.query(query, {
+            replacements: {
+                id: id
+            }
+        });
+
         return { id };
     }
 
