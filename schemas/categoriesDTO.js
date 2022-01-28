7f4120b9e2f5e6179af6273a9ab6856415b1a196
Omar M Juarez <omarmju906p@hotmail.com>
2022-01-27 19:17:55 -0600

    No comments on Categories.

--- schemas/categoriesDTO.js
+++ schemas/categoriesDTO.js
@@ -2,7 +2,6 @@ const joi = require("joi");
 
 const id = joi.number().integer();
 const name = joi.string().max(80);
-// const items = joi.number().integer();
 
 const getCategorySchema = joi.object({
     id: id.required()
@@ -10,12 +9,10 @@ const getCategorySchema = joi.object({
 
 const createCategorySchema = joi.object({
     name: name.required()
-    // items: items
 });
 
 const updateCategorysSchema = joi.object({
     name: name,
-    // items: items
 });
 
 module.exports = { getCategorySchema, createCategorySchema, updateCategorysSchema };
